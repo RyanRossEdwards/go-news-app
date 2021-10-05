e513951127eef74f97e3702aa572a0aab612f7cb
Ryan Edwards <37044278+RyanRossEdwards@users.noreply.github.com>
2021-10-05 17:57:33 +1100

    Update main.go

--- main.go
+++ main.go
@@ -5,4 +5,24 @@ import (
 	"os"
 )
 
-func main {}
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello World!</h1>"))
+}
+
+func main {
+	// attempt to set the port variable based on the PORT environment variable
+	port := os.Getenv("PORT")
+	// Getenv returns empty string if environment variable not present
+	if port == "" {
+		port = "3000"
+	}
+
+	// creates an HTTP request multiplexer "mux"
+	mux := http.NewServeMux()
+
+	// HandleFunc variables:
+	// 1 - pattern string
+	// 2 - func(w http.ResponseWriter, r *http.Request)
+	mux.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":"+port, mux)
+}
